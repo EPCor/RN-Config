98b141f0d75c493b17d6899576efb5b7fe4b5e23
zhilidali <zhilidali@qq.com>
2020-09-09 05:32:23 +0800

    path aliases

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'babel-plugin-module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '~': './src',
+          '@test': './__test__',
+          '@': './',
+        },
+      },
+    ],
+  ],
 };
